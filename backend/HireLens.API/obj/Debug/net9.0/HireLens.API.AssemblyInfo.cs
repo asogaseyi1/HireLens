756852c845613fd323f2b166ec7ffc34fa1a02c7
Seyi <asogaseyi1@gmail.com>
2025-06-18 08:04:01 -0500

    build update

--- backend/HireLens.API/obj/Debug/net9.0/HireLens.API.AssemblyInfo.cs
+++ backend/HireLens.API/obj/Debug/net9.0/HireLens.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HireLens.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41049dac5b7204ab50f0f966c6ae480b9c88fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847e059fd3aea17b2c14aee14297867a02a6c7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HireLens.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HireLens.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
